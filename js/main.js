f8beb25ea43001954549c770656a6f12ee69202a
Harrison <harrison@theshoebridges.com>
2015-05-02 15:30:58 +1000

    js/main: add basic routes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 var app = angular.module('mentor-app', ['ngRoute']);
 app.config(['$routeProvider',
-          function($routeProvider) {
-              $routeProvider.
-                when('/', {
-                        templateUrl: 'templates/mentor.html',
-                        controller: 'rootController'
-                      });
-              }]);
-console.log("HELLO WORLD");
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'templates/home.html',
+                controller: 'rootController'
+            }).
+            when('/confirm', {
+                templateUrl: 'templates/confirm.html'
+            }).
+            when('/form', {
+                templateUrl: 'templates/form.html'
+            }).
+            when('/mentor', {
+                templateUrl: 'templates/mentor.html'
+            }).
+            when('/mentors', {
+                templateUrl: 'templates/mentor_list.html'
+            });
+}]);
