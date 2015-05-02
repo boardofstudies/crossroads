424a13ae01b16288d3dde9200450264ae8a3342b
Nathan Feiglin <nathanfeiglin@gmail.com>
2015-05-02 16:36:01 +1000

    adds some dodgy data stuff

--- js/main.js
+++ js/main.js
@@ -19,3 +19,17 @@ app.config(['$routeProvider',
                 templateUrl: 'templates/mentor_list.html'
             });
 }]);
+
+function Global(){
+	function config() {
+		var data = {
+			outline: [
+				{
+					profile_img: 'https://profileimggoeshere',
+					name: 'Judge 1',
+					bio: 'Bio bio bio bio'
+				}
+			]
+		};
+	}
+};
